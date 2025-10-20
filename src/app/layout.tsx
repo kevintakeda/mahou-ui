d34dc0039decb5559ac5fa6f4737adda325e06fd
Kevin <9105388+kevintakeda@users.noreply.github.com>
2025-10-20 03:14:05 -0300

    add metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,16 @@
 import "@/app/global.css";
 import { RootProvider } from "fumadocs-ui/provider/next";
 import { Inter } from "next/font/google";
-import Head from "next/head";
 
 const inter = Inter({
   subsets: ["latin"],
 });
+export const metadata = {
+  title: {
+    default: "Mahou UI",
+    template: "%s - Mahou UI",
+  },
+};
 
 export default function Layout({ children }: LayoutProps<"/">) {
   return (
